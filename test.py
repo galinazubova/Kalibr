008f13ea3af9b9cca3687e898dfe25ef26e2fd28
SvitEM <Svyatoslav.EM@gmail.com>
2017-02-25 22:16:19 +0300

    For OpenCv test
    If you sucsess install opencv, you'l see lollipop)

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
-print('hello git!')
+import numpy as np
+import cv2
+
+img = cv2.imread('lollipop.jpg',0)
+cv2.imshow('image',img)
+k = cv2.waitKey(0)
+if k == 27:         # wait for ESC key to exit
+    cv2.destroyAllWindows()
+
